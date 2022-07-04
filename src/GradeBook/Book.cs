db4c85de1bd03cd7a79ef6d0bbbc9701180fbbd7
Ruth Waiganjo <ruthwaiganjo@microsoft.com>
2022-07-04 09:38:48 +0300

    modifiers

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,28 @@
+﻿namespace GradeBook
+{
+	 class Book
+	{
+		public Book(string name)
+        {
+            grades = new List<double>();
+			// this(object)
+            this.name = name;
+        }
+
+        // Instance member/ Instance method
+        public void AddGrade(double grade)
+		{
+			grades.Add(grade);
+		}
+
+		// A field accessible throughout the methods in the class
+		// A field is declared in a class or struct. A data member of a class
+		List<double> grades;
+		private string name;
+	}
+}
+
+/* Access modifiers; public(code outside of the class can have access to it)
+ * In OOP, you are protective of your state. You don't want to allow direct access to modifying stored values
+ * Private: Field available only in the class 
+*/
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,24 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-if(args.Length > 0 )
-{
-    Console.WriteLine($"Hello, {args[0]} !");
-}
-else
-{
-    var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
-    var result = 0.0;
 
-    //List<double> grades = new List<double>();
-    var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-    grades.Add(56.1);
-
-    foreach(double number in grades)//numbers
+namespace GradeBook
+{
+    class Program
     {
-        result += number;
-    }
-    var average = result / grades.Count;
+        static void Main(string[] args)
+        {
+            var book = new Book("Ruth's Grade Book");
+            book.AddGrade(89.1);
 
-    Console.WriteLine($"The sum is: {result}");
-    Console.WriteLine($"The average is: {average:N1}");
+
+            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            var result = 0.0;
+
+            //List<double> grades = new List<double>();
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            foreach(double number in grades)//numbers
+            {
+                result += number;
+            }
+            var average = result / grades.Count;
+
+            Console.WriteLine($"The sum is: {result}");
+            Console.WriteLine($"The average is: {average:N1}");
+        }
+     }
 }
+
