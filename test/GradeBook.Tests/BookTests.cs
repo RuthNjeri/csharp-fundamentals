1273536d4eed87294685d642311e52f742d8dfa4
Ruth Waiganjo <ruthwaiganjo@microsoft.com>
2022-07-07 10:36:48 +0300

    unit testing

---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,26 @@
+namespace GradeBook.Tests;
+
+// Test execution and discovery happens when you run the test runner
+public class BookTests
+{
+    [Fact] 
+    /*
+       An attribute. Little piece of data that ia attached to the symbol that follows it. [Fact] is attached to Test1.
+       The test methods are identified by the [Fact] attribute.
+       Attributes are also described as decorations that we hang on sth like a method
+    */
+    public void Test1() // A method has to be a member of a type. Test1 is a member of Type: Class UnitTest1
+    {
+        /* Typically most people break a test into 3 sections
+         * 1. Arrange section
+         *      Put together all your test data and arrange the objects and values that you are going to use   
+         */
+        var book = new Book("");
+
+        // 2. Act section - Invoke a method to perform a calculation or produce a result
+        var actual = x + y;
+
+        // 3. Assert section - Assert sth about the value that was computed
+        Assert.Equal(expected, actual);
+    }
+}
